82b1ef6868e0e8a1d21944fe09eb16b4a3291178
miral_kashiwagi <36557632+miralkashiwagi@users.noreply.github.com>
2026-09-16 09:37:05 +0900

    Update versionCode and versionName in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.example.piyologtimewidget"
         minSdk = 26
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 1
+        versionName = "1"
     }
 
     buildTypes {
